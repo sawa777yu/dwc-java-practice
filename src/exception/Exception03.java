bf0a678dea39df76bfc84114b8c6c662f7118bc1
yuya sawata <sawatayuya@sawadayuuyanoMacBook-Air.local>
2021-12-30 15:41:44 +0900

    例外処理

--- src/exception/Exception03.java
+++ src/exception/Exception03.java
@@ -3,12 +3,16 @@ package exception;
 public class Exception03 {
 	public static void main(String[] args) {
 		try {
+//			division変数の定義はこの時点ではまだされていないのになぜ使えるのか
 			Exception03.division(100, 0);
+//		「catch(例外クラス　変数)」という公式
 		}catch (ArithmeticException e) {
 			System.out.println("ArithmeticException例外が発生");
 		}catch (IllegalArgumentException e) {
 			System.out.println("IllegalArgumentException例外が発生");
-//			throw e;
+//			「throw 例外インスタンス」で例外インスタンスをスローする。
+//			下記のeはcatchで定義した変数のe。catchした例外インスタンスをスローしている
+			throw e;
 		}
 		System.out.println("プログラム終了");
 	}
@@ -16,6 +20,7 @@ public class Exception03 {
 	public static void division(int a, int b) {
 		System.out.println(a + " ÷ " + b + " は？ ");
 		if (b == 0) {
+//			「throw new 例外クラス」で例外インスタンスを生成してスローする。
 			throw new IllegalArgumentException("引数が不正です");
 		}
 		int result = a / b;
