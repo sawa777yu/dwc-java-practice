80be6b6c94db4c261e44653f7f4f299a416019d9
sawa777yu <sawa.oranger7@gmail.com>
2022-01-11 19:32:12 +0900

    Chapter2~4の復習

---
+++ src/Chapter02re.java
@@ -0,0 +1,7 @@
+
+public class Chapter02re {
+	public static void main(String[] args) {
+		System.out.println("Hello WEBCAMP");
+		System.out.println("Hello Java");
+	}
+}
---
+++ src/Chapter04re.java
@@ -0,0 +1,14 @@
+
+public class Chapter04re {
+	public static void main(String[] args) {
+		String str = "sawa";
+		System.out.println(str);
+		
+		str = "睡眠";
+		System.out.println(str);
+		
+//		定数は大文字で書くこと
+		final int NUM = 7;
+		System.out.println(NUM);
+	}
+}
